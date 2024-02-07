bac72975da7a333ceb65159305f1ab40564015ec
manojui <30927589+manojui@users.noreply.github.com>
2024-02-07 14:13:00 -0600

    Added spring security for login to the application

--- spring-boot-crud-operation/src/main/java/com/po/SpringBootCrudOperationApplication.java
+++ spring-boot-crud-operation/src/main/java/com/po/SpringBootCrudOperationApplication.java
@@ -1,7 +1,10 @@
 package com.po;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 @SpringBootApplication
+@EnableWebSecurity
 public class SpringBootCrudOperationApplication 
 {
 public static void main(String[] args) 
